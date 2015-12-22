056ebd40d4a48d27cc7b0bfc6c548799b83b5ff8
Esteban Sánchez <esteban.sanchez@gmail.com>
2015-12-22 10:13:10 +0100

    Disabled apache2 modules and installed extra php5 modules

--- part01/cookbooks/kodify/recipes/3-apache-php.rb
+++ part01/cookbooks/kodify/recipes/3-apache-php.rb
@@ -1,5 +1,30 @@
 package "apache2"
 
-package "php5" do
-    version '>=5.4'
+disabled_modules = [
+    "autoindex",
+    "dir",
+    "status",
+]
+
+disabled_modules.each do |m|
+    execute "disable module" do
+        command "a2dismod #{m}"
+    end
+end
+
+
+# I know it offers at least PHP 5.5 and
+# I couldn't specify something like >=5.4
+# because chef does it owns tricks wit this
+package "php5"
+
+extra_packages = [
+    "php5-mysql",
+    "php5-xcache",
+    "php5-mongo",
+    "libapache2-mod-php5",
+]
+
+extra_packages.each do |p|
+    package p
 end
